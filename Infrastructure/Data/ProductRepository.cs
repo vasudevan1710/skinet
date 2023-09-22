a4bfb0da76537edc2ebc8d173e6e7cd395cabc14
Vasu <vasude1710@gmail.com>
2023-09-22 08:23:35 +0530

    End of section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -28,7 +28,7 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await this.context.Products
+           return await this.context.Products
             .Include( p=> p.ProductType)
             .Include(p=> p.ProductBrand)
             .ToListAsync();
---
+++ core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+using core.Entities;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using core.Specifications;
+
+namespace core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    }
+}
